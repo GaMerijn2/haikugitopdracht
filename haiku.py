e093e1ce95f5fe905b28f15739c4cadd8cedcfa6
Merijn Florie <34626@ma-web.nl>
2022-10-13 14:27:23 +0200

    Haiku Nummer 1

--- haiku.py
+++ haiku.py
@@ -1,11 +1,27 @@
 
 
 #plak hier de haiku functions onder elkaar
+def Haiku1():
+    print("haiku by:","Merijn")
+    print("Ik heb honger.")
+    print("Geef mij eten.")
+    print("anders haal je eten.")
 
+def Haiku2():
+    print("haiku by:","AUTHOR")
+    print("REGEL1")
+    print("REGEL2")
+    print("REGEL3")
 
-
+def Haiku3():
+    print("haiku by:","AUTHOR")
+    print("REGEL1")
+    print("REGEL2")
+    print("REGEL3")
 
 #laat de code hieronder met rust
 Haiku1()
+print(" ")
 Haiku2()
+print(" ")
 Haiku3()
